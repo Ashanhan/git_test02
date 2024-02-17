60c47c963fc73ff3cb5a0dc0085b06ba3131cf32
csc <1210184090@qq.com>
2024-02-17 08:39:23 +0800

    firstcommithot

--- src/main/java/com/csc/Test.java
+++ src/main/java/com/csc/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello git222222");
+        System.out.println("hot-fix test111");
     }
 }
